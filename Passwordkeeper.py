f1d0a5b03a20a4d452fa69d7c1fa5724bb84e0f1
Faaris Ahmed <97902575+faarisaahmed@users.noreply.github.com>
2022-05-24 18:02:28 -0400

    Update Passwordkeeper.py

--- Passwordkeeper.py
+++ Passwordkeeper.py
@@ -1,3 +1,5 @@
 print ("Welcome to your password keeping app! Now type in a password to get started.")
 password=input ("Please enter your password here> ")
 print ("Good. You will have to remember that so you can see your passwords.")
+username=input ("Now, please type in your name> ")
+print (f"Well {username}, in this password keeping app, can do a couple of things. To add a new password, type in new password and enter your brand new password. To add a new username, type in new username and enter your brand new username.")
