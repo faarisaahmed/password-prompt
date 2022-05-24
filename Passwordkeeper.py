e38c8f6127a89bab9c608a36013c728b6fb2ac41
Faaris Ahmed <97902575+faarisaahmed@users.noreply.github.com>
2022-05-24 16:23:18 -0400

    Create Passwordkeeper.py

---
+++ Passwordkeeper.py
@@ -0,0 +1,3 @@
+print ("Welcome to your password keeping app! Now type in a password to get started.")
+password=input ("Please enter your password here> ")
+print ("Good. You will have to remember that so you can see your passwords.")
