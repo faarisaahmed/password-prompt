1c136b4a6d24fc343c3ba9a7154e4c7f4555715b
Faaris Ahmed <97902575+faarisaahmed@users.noreply.github.com>
2022-05-25 14:48:44 -0400

    Update Passwordkeeper.py

--- Passwordkeeper.py
+++ Passwordkeeper.py
@@ -2,7 +2,10 @@ print ("Welcome to your password keeping app! Now type in a password to get star
 password=input ("Please enter your password here> ")
 print ("Good. You will have to remember that so you can see your passwords.")
 username=input ("Now, please type in your name> ")
-print (f"Well {username}, in this password keeping app, you can do a couple of things. To add a new password, type in new password and enter your brand new password. To add a new username, type in new username and enter your brand new username. You also have to give each one a name. Now to search your password/username, type in the name of it and you can see it. Good. Well, now you know how to use this app. Continue!")
-search=input ("Type in new password, new username, or search an existing username/password.")
+print (f"Well {username}, in this password keeping app, you can do a couple of things. To add a new password, type in new password and enter the website name that you are adding for. Then add the password for it. To add a new username, type in new username and enter the website name that you are adding for. Then add the username for it. Now to search your password/username, type in the name of it and you can see it. Good. Well, now you know how to use this app. Continue!")
+search=input ("Type in new password, new username, or search an existing username/password. ")
 if search.upper()=="NEW PASSWORD":
-    print("ok fine we'll do a new password")
+    for _ in range(3):
+        sitename=input("Ok. To add a password, I will ask you some questions. What is the website you are adding your password for? ")
+        sitepass=input(f"What is your password for {sitename}? ")
+        allpasswords[sitename]=sitepass
