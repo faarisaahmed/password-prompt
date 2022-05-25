51e41a6c5ce3c11fe5a09682018e83429366e290
Faaris Ahmed <97902575+faarisaahmed@users.noreply.github.com>
2022-05-25 15:23:40 -0400

    Update Passwordkeeper.py

--- Passwordkeeper.py
+++ Passwordkeeper.py
@@ -1,11 +1,20 @@
+allpasswords = {}
+
 print ("Welcome to your password keeping app! Now type in a password to get started.")
 password=input ("Please enter your password here> ")
 print ("Good. You will have to remember that so you can see your passwords.")
 username=input ("Now, please type in your name> ")
 print (f"Well {username}, in this password keeping app, you can do a couple of things. To add a new password, type in new password and enter the website name that you are adding for. Then add the password for it. To add a new username, type in new username and enter the website name that you are adding for. Then add the username for it. Now to search your password/username, type in the name of it and you can see it. Good. Well, now you know how to use this app. Continue!")
-search=input ("Type in new password, new username, or search an existing username/password. ")
-if search.upper()=="NEW PASSWORD":
-    for _ in range(3):
-        sitename=input("Ok. To add a password, I will ask you some questions. What is the website you are adding your password for? ")
-        sitepass=input(f"What is your password for {sitename}? ")
+
+for _ in range(1000):
+    search=input ("\n Type in [New Login] [Search Logins] ")
+    if search.upper()=="NEW LOGIN":
+        sitename=input("New Login. Okay, for which website? ")
+        sitepass=input(f"Got it. what is your password for {sitename}? ")
         allpasswords[sitename]=sitepass
+        
+    if search.upper()=="SEARCH LOGINS":
+        sitename=input("Okay, so which website's logging do you want? ")
+        masterpass=input(f"Got it, what is your master password? ")
+        if masterpass==password:
+            print(f"The password for {sitename} is {allpasswords[sitename]}.")
