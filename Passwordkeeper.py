cc0743f93b6c18639cbbd254b0ec2d76138624d7
Saif Ahmed <saif.r.ahmed@gmail.com>
2022-05-25 14:24:39 -0400

    Update Passwordkeeper.py

--- Passwordkeeper.py
+++ Passwordkeeper.py
@@ -4,4 +4,5 @@ print ("Good. You will have to remember that so you can see your passwords.")
 username=input ("Now, please type in your name> ")
 print (f"Well {username}, in this password keeping app, you can do a couple of things. To add a new password, type in new password and enter your brand new password. To add a new username, type in new username and enter your brand new username. You also have to give each one a name. Now to search your password/username, type in the name of it and you can see it. Good. Well, now you know how to use this app. Continue!")
 search=input ("Type in new password, new username, or search an existing username/password.")
-if search==
+if search.upper()=="NEW PASSWORD":
+    print("ok fine we'll do a new password")
